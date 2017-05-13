94065bf6444e41c0c9d3a95444c434ad7c981f9a
龔彥賓 <Ben@gongyanbinde-MacBook-Pro.local>
2017-05-13 13:06:23 +0800

    OK

--- src/app/Login.component.ts
+++ src/app/Login.component.ts
@@ -4,26 +4,26 @@ import {LoginService} from './services/Login.service'
  import {LoginModel} from './AuthGuard/LoginModel'
 @Component({
   selector: 'login',
-  template: '<h1>{{title}}</h1>  <button (click)="login()">fffff</button>',
+  template: '<h1>{{title}}</h1>  <button (click)="login()">Login</button>',
   styleUrls: ['./app.component.css']
 })
 export class LoginComponent {
   title = 'LoginPage';
   constructor(private router:Router,private loginservice:LoginService){}
   OnInit(){
-     
+
   }
   login() {
     let  data:LoginModel={Username:"ben",Password:"1"};
-            
+
      console.log(data)
     this.loginservice.login(data)
-            .subscribe(result => {      
+            .subscribe(result => {
                 if (result === true) {
                     // login successful
                     this.router.navigate(['/']);
                 } else {
-                  
+
                   confirm("帳密錯誤")
                 }
             });
