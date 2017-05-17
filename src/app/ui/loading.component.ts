8be9f11ebed3fe369aef8a17878969b0aae08a49
龔彥賓 <Ben@gongyanbindeMBP.mshome.net>
2017-05-17 11:14:14 +0800

    update loading spinner

--- src/app/ui/loading.component.ts
+++ src/app/ui/loading.component.ts
@@ -3,9 +3,8 @@ import {Component} from '@angular/core'
 @Component({
     selector:"Loading",
     template:`
-  <pre>Loading</pre>
 
-<!--    <img src="C://Users//smart//Desktop//angulartest//src//app//ui//image//default.gif">-->
+   <img src="../assets/images/LoadingSpinner.gif">
     `,
 })
 export class LoadingComponent{
