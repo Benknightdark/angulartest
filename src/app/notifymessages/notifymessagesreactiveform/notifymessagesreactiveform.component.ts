a437b594ada9d39925b754cccecf578f6fadbd98
龔彥賓 <Ben@gongyanbindeMBP.mshome.net>
2017-05-22 17:42:30 +0800

    binding model

--- src/app/notifymessages/notifymessagesreactiveform/notifymessagesreactiveform.component.ts
+++ src/app/notifymessages/notifymessagesreactiveform/notifymessagesreactiveform.component.ts
@@ -1,9 +1,9 @@
-import {Messages} from '../notifymessagesform/Messages';
+import { Messages } from '../notifymessagesform/Messages';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router, Params, ActivatedRoute } from '@angular/router';
-import {Observable} from 'rxjs'
-import {NotifymessagesService} from '../../services/notifymessages.service';
+import { Observable } from 'rxjs'
+import { NotifymessagesService } from '../../services/notifymessages.service';
 
 @Component({
   selector: 'app-notifymessagesreactiveform',
@@ -11,13 +11,14 @@ import {NotifymessagesService} from '../../services/notifymessages.service';
   styleUrls: ['./notifymessagesreactiveform.component.css']
 })
 export class NotifymessagesreactiveformComponent implements OnInit {
- id: string;
-  title: string
-  DetailData:Observable<Messages>
-  DetailDataForm:FormGroup;
-  constructor(private router: Router, private fb: FormBuilder,private route: ActivatedRoute,private mservice:NotifymessagesService) {
+  id: string;
+  title: string;
+  isEdit: boolean;
+  DetailData: Observable<Messages>
+  DetailDataForm: FormGroup;
+  constructor(private router: Router, private fb: FormBuilder, private route: ActivatedRoute, private mservice: NotifymessagesService) {
 
-   }
+  }
 
   ngOnInit() {
     this.title =
@@ -25,20 +26,26 @@ export class NotifymessagesreactiveformComponent implements OnInit {
         : this.router.url.indexOf("Edit") > 0 ? "EditPage"
           : this.router.url.indexOf("Create") > 0 ? "CreatePage"
             : "";
-
+    this.isEdit = this.router.url.indexOf("Detail") > 0 ? true : false;
     this.route.params.subscribe(p => { this.id = p["id"]; console.log(this.id); });
-    this.mservice.GetNotifyMessagesDetail(this.id).subscribe(res=>this.DetailData=res[0])
-     this.DetailDataForm=this.fb.group({
-      title: '',
-      body: '',
-      SendTime: ''
+
+    this.mservice.GetNotifyMessagesDetail(this.id).subscribe(res => this.DetailData = res[0])
+
+    this.DetailDataForm = this.fb.group({
+      title: ["", Validators.compose([Validators.required])],
+      body: ["", Validators.compose([Validators.required])],
+      SendTime: ["", Validators.compose([Validators.required])]
     });
+    if(this.isEdit){
+    this.DetailDataForm.disable();
+  }
+  console.log(this.DetailDataForm)
 
   }
   onBack() {
     this.router.navigate(['/NotifyMessages']);
   }
 
-  onSubmit() {  }
+  onSubmit() { console.log(this)}
 
 }
